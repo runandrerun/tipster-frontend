80c08cd3477086463b7e994c1e6419281059fae3
runandrerun <the.asantiagojr@gmail.com>
2018-11-12 06:53:43 -0500

    CurrencyCard component setup

---
+++ src/components/CurrencyCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class CurrencyCard extends Component {
+  render() {
+    return (
+      <div className="Currency-Card">
+        Hi
+      </div>
+    );
+  }
+}
+
+export default CurrencyCard;
