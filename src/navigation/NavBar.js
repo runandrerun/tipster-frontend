4ff5d74d19533690ebad94658f194784dbf077f1
runandrerun <the.asantiagojr@gmail.com>
2018-11-12 19:06:25 -0500

    Navbar adjustments

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import '../styles/nav.css';
 
 const styles = {
   root: {
@@ -16,7 +17,7 @@ function SimpleAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="inherit">
+      <AppBar position="static" style={{ backgroundColor: "#2096F3" }}>
         <Toolbar>
           <Typography variant="title" color="inherit">
             Travel Tipster
